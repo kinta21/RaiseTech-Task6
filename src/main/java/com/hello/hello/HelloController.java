3f61eacd15784240cb5220c34c24d5c6da6623b0
kinta21 <kinta21127@yahoo.ne.jp>
2023-08-20 16:47:03 +0900

    HelloWorldの後に、現在日時を追加

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -3,11 +3,17 @@ package com.hello.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-      @GetMapping("/hello")
-      public String hello(){
-         return "Hello World!!";
-      }
+@RestController
+public class HelloController{
+
+    @GetMapping("/hello")
+    public String hello() {
+        LocalDateTime nowDate = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(" yyyy年MM月dd日  E曜日　HH時mm分ss秒");
+        String formatNowDate = formatter.format(nowDate);
+        return "Hello World!!　 日本は現在" + formatNowDate;
+    }
 }
